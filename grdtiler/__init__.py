ecb3f3ca3606355fd64782ecca5352136de762d9
Jean Renaud <jrmiadan@ifremer.fr>
2024-03-07 16:38:22 +0100

    Adding logging info to show logging start and end

--- grdtiler/__init__.py
+++ grdtiler/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Jean Renaud MIADANA"""
 __email__ = "jrenaud495@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = ['tiling_prod', 'tiling_by_point', 'tiles_images', 'get_tiles_footprint', 'plot_cartopy_data']
 
 from .grdtiler import *
