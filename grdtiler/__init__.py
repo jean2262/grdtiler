96c76a100c06031cec452b97c8d5c642781da394
Jean Renaud <jrmiadan@ifremer.fr>
2024-08-07 09:05:02 +0200

    update

--- grdtiler/__init__.py
+++ grdtiler/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Jean Renaud MIADANA"""
 __email__ = "jrenaud495@gmail.com"
-__all__ = ['tiling_prod', 'tiling_by_point', 'plot_cartopy_data']
+__all__ = ['tiling_prod', 'tiling_by_point']
 
 from .grdtiler import *
 from .tools import *
